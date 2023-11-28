065099b5da200714781cecaaf3f7593c1b3ed2fd
refleks1a <145009608+refleks1a@users.noreply.github.com>
2023-11-28 23:27:55 +0400

    Remove Django-secret-key from settings.py

--- MultiApp_application/multiapp_application/multiapp_application/settings.py
+++ MultiApp_application/multiapp_application/multiapp_application/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#fuiadnt@sqgwc4y3s$3!g^bcq&oupmz13ge$ibshn)1i$(6y#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
